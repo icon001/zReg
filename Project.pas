d372baf3b22b603a398b3b6962dfb70a24b2659b
icon001 <icon001@naver.com>
2018-04-10 11:33:38 +0900

    20180410 zReg 등록

---
+++ Project.pas
@@ -0,0 +1,19 @@
+{ Unit created by Compile1 [TCompile] component on 2013.05.03 }
+unit Project;
+
+interface
+
+const { Do not modify these lines }
+  PROJECT_NAME: string = '';
+  EXE_FILENAME: string = 'zReg';
+  DATE_CREATED: string = '2013.05.03';
+  AUTHORS_NAME: string = '';
+  CONTACT_INFO: string = '';
+  COMPILE_DATE: string = '2018.03.22';
+  COMPILE_TIME: string = 'pm 01:10:16';
+  BUILD_NUMBER: string = '483';
+  UNIQUE_IDKEY: string = ' 2018.03.22 pm 01:10:16';
+
+implementation
+
+end.
